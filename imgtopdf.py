c15e0cf6911f37a8145c5b68b21e299cf6a571fc
Daniel Andrade <156219890+danielandradestudy@users.noreply.github.com>
2026-02-04 13:29:29 -0300

    Update image uploader prompt in imgtopdf.py

--- imgtopdf.py
+++ imgtopdf.py
@@ -9,7 +9,7 @@ st.title("🖼️ Imagem para PDF")
 st.write("Suba sua imagem e baixe o PDF convertido instantaneamente.")
 
 # Widget de Upload
-uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Escolha uma imagem... E é só uma mesmo viu?", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     # Exibe uma prévia da imagem
@@ -37,3 +37,4 @@ if uploaded_file is not None:
         )
 
         st.success("Conversão concluída com sucesso!")
+
